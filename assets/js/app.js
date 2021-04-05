b101c6e7a47fed3fd7477c1357cdc9b4a4aec5f8
kevin cortes <kevinkannzz@gmail.com>
2021-04-05 00:54:00 -0500

    Se agrego contador a articulos agregados al carrit

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,8 @@ var productos = {
 
 }
 
+var carneUnoprecio = 200;
+
 function mostrarProductos() {
     console.log(productos[2].precio.precioNormal);
 }
@@ -33,3 +35,83 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function meatUno() {
+    var x = document.getElementById("mySelect");
+    var option = document.createElement("option");
+
+
+    option.text = "Meat #1";
+    var btn = document.createElement("button");
+    btn.innerHTML = "";
+    option.appendChild(btn);
+
+    x.add(option);
+
+
+
+
+    var precio1 = 400;
+    document.getElementById("totalNum").innerHTML = "$ " + precio1;
+
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+
+}
+
+
+function meatDos() {
+    var x = document.getElementById("mySelect");
+    var option = document.createElement("option");
+
+    option.text = "Meat #2";
+
+    x.add(option);
+
+
+    var precio2 = 300;
+    document.getElementById("totalNum").innerHTML = "$ " + precio2;
+
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+function meatTres() {
+    var x = document.getElementById("mySelect");
+    var option = document.createElement("option");
+    option.text = "Meat #3";
+    x.add(option);
+
+    var precio3 = 100;
+    document.getElementById("totalNum").innerHTML = "$ " + precio3;
+
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+
+    return precio3
+}
+
+
+
+function vaciar() {
+    var select = document.getElementById("mySelect");
+    var length = select.options.length;
+    for (i = length - 1; i >= 0; i--) {
+        select.options[i] = null;
+    }
+    var precio = 0;
+    document.getElementById("totalNum").innerHTML = "$ " + precio;
+
+    clicks = 0;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+var clicks = 0;
+
+
+
+
+
+
+var precio = 0;
+document.getElementById("totalNum").innerHTML = "$ " + precio;
